e8ca3e128d0976da12a2054d7c18efdf02eadf13
evanafridianto <evanstore620@gmail.com>
2024-06-23 16:48:39 +0700

    update

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 3.2.0
     </div>
-    <strong>Copyright &copy; {{ date('Y') }} Scrapper.</strong> All
+    <strong>Copyright &copy; {{ date('Y') }} Scraper.</strong> All
     rights
     reserved.
 </footer>
